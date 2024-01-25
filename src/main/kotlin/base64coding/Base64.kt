60f5550703a32ce841b5e02310d770f355bb81ea
Šimon Kocúrek <simon.kocurek@gmail.com>
2024-01-25 15:58:33 +0100

    Add placeholder for base64 encoding and decoding utilities

---
+++ src/main/kotlin/base64coding/Base64.kt
@@ -0,0 +1,17 @@
+package base64coding
+
+class Base64DecodingException(message: String) : IllegalArgumentException(message)
+
+/**
+ *
+ */
+fun encodeBase64(value: ByteArray): String {
+    return ""
+}
+
+/**
+ *
+ */
+fun decodeBase64(value: String): ByteArray {
+    return ByteArray(0)
+}
