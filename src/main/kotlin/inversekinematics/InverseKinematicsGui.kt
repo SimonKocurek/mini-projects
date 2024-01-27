e2a46ca360a914d0a8b380437f1f8ece77109240
Šimon Kocúrek <simon.kocurek@gmail.com>
2024-01-27 11:45:42 +0100

    Update IK GUI based on IK Solver

--- src/main/kotlin/inversekinematics/InverseKinematicsGui.kt
+++ src/main/kotlin/inversekinematics/InverseKinematicsGui.kt
@@ -1,6 +1,6 @@
 package inversekinematics
 
-import java.awt.Graphics
+import java.awt.*
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionListener
 import java.awt.image.BufferedImage
@@ -8,26 +8,35 @@ import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
 
+private const val X_SIZE = 600
+private const val Y_SIZE = 600
+
 fun main(args: Array<String>) {
     SwingUtilities.invokeLater {
-        val ik = InverseKinematics()
-        InverseKinematicsGui(ik)
+        // Inverse kinematics is used for figuring out how an arm consisting of many
+        // segments/bones/joints should rotate so that the final point reaches the
+        // desired position.
+        val armPoints = (0..6).map { Point2D(X_SIZE / 2, Y_SIZE - it * 75) }
+        val inverseKinematics = InverseKinematics()
+        InverseKinematicsGui(inverseKinematics, armPoints)
     }
 }
 
 class InverseKinematicsGui(
-    inverseKinematics: InverseKinematics
+    inverseKinematics: InverseKinematics,
+    armPoints: List<Point2D>
 ) : JFrame("Inverse Kinematics Demo") {
 
     init {
-        setSize(600, 600)
+        setSize(X_SIZE, Y_SIZE)
         setLocationRelativeTo(null)
         isResizable = false
         defaultCloseOperation = EXIT_ON_CLOSE
 
         val paintPanel = InverseKinematicsPaintPanel(
             inverseKinematics = inverseKinematics,
-            bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+            bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB),
+            armPoints = armPoints
         )
         contentPane.add(paintPanel)
 
@@ -37,16 +46,15 @@ class InverseKinematicsGui(
 
 private class InverseKinematicsPaintPanel(
     private val inverseKinematics: InverseKinematics,
-    private val bufferedImage: BufferedImage
+    private val bufferedImage: BufferedImage,
+    private var armPoints: List<Point2D>
 ) : JPanel(), MouseMotionListener {
 
-    private val TARGET_FRAME_MS = 16L
-
-    private var lastX = 0
-    private var lastY = 0
+    private val targetFrameMs = 16L
 
     init {
         addMouseMotionListener(this)
+        repaintArm()
     }
 
     override fun paintComponent(graphics: Graphics) {
@@ -54,18 +62,37 @@ private class InverseKinematicsPaintPanel(
         graphics.drawImage(bufferedImage, 0, 0, this)
     }
 
-    override fun mouseDragged(e: MouseEvent) {
-        val graphics = bufferedImage.createGraphics()
-        graphics.drawLine(lastX, lastY, e.x, e.y)
-        lastX = e.x
-        lastY = e.y
-        graphics.dispose()
+    override fun mouseDragged(e: MouseEvent) = onMouseEvent(e)
 
-        repaint(TARGET_FRAME_MS)
+    override fun mouseMoved(e: MouseEvent) = onMouseEvent(e)
+
+    private fun onMouseEvent(e: MouseEvent) {
+        armPoints = inverseKinematics.rotateArmToReach(
+            armPoints = armPoints,
+            destination = Point2D(x = e.x, y = e.y)
+        )
+        repaintArm()
     }
 
-    override fun mouseMoved(e: MouseEvent) {
+    private fun repaintArm() {
+        val graphics = bufferedImage.graphics as Graphics2D
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 
+        graphics.clearRect(0, 0, width, height)
+        armPoints.zipWithNext().reversed().forEachIndexed { index, (current, next) ->
+            graphics.color = Color.getHSBColor(index / 25.0f, 0.5f, 1.0f)
+            graphics.stroke = BasicStroke(3f + index * 1.25f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND)
+
+            graphics.drawLine(
+                current.x,
+                current.y,
+                next.x,
+                next.y
+            )
+        }
+        graphics.dispose()
+
+        repaint(targetFrameMs)
     }
 
 }
