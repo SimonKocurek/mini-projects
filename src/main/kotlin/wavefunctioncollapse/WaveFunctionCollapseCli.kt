c160f5141f5599667bbdf8457654cd3462bc07c8
Šimon Kocúrek <simon.kocurek@gmail.com>
2024-03-01 18:15:09 +0100

    Add first wave function collapse step description

--- src/main/kotlin/wavefunctioncollapse/WaveFunctionCollapseCli.kt
+++ src/main/kotlin/wavefunctioncollapse/WaveFunctionCollapseCli.kt
@@ -1,6 +1,9 @@
 package wavefunctioncollapse
 
 import com.github.ajalt.clikt.core.CliktCommand
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.io.File
 import javax.imageio.ImageIO
 
@@ -11,8 +14,34 @@ fun main(args: Array<String>) {
 class WaveFunctionCollapse: CliktCommand(
     name = "wavefunctioncollapse",
 ) {
-    override fun run() {
-        ImageIO.read(File("Test.png"))
+
+    private val kernelSize = 3
+
+    override fun run(): Unit = runBlocking(Dispatchers.Default) {
+        val image = withContext(Dispatchers.IO) { ImageIO.read(File("Test.png")) }
+
+        for (y in 0..image.height - kernelSize) {
+            for (x in 0..image.width - kernelSize) {
+                image.getSubimage(x, y, kernelSize, kernelSize)
+
+                // original
+                // Rotate 90
+                // Rotate 180
+                // Rotate 270
+                // horizontal flip
+                // vertical flip
+
+            }
+        }
+
+        // Find unique patterns, their count and Assign IDs
+
+        // Find overlaps
+        // top 6
+        // bottom 6
+        // left 6
+        // right 6
+
     }
 
 }
